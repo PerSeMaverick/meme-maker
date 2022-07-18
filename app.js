51dbe856af22da8dabe0afa3e4374c6452ab6266
PerSeMaverick <skhu151827@gmail.com>
2022-07-18 21:35:48 +0900

    drawing board prototype

--- app.js
+++ app.js
@@ -3,3 +3,27 @@ const ctx = canvas.getContext("2d");
 
 canvas.width = 700;
 canvas.height = 700;
+ctx.lineWidth = 2;
+
+const colors = [
+  "#cd84f1",
+  "#ffcccc",
+  "#ff4d4d",
+  "#ffaf40",
+  "#fffa65",
+  "#32ff7e",
+  "#7efff5",
+  "#17c0eb",
+  "#7158e2",
+  "#3d3d3d",
+];
+
+function onMousemove(event) {
+  ctx.beginPath();
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  ctx.strokeStyle = color;
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+}
+
+canvas.addEventListener("mousemove", onMousemove);
